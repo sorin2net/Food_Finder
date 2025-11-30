3b2845ca8cffc7613581b36b6175703499145524
sorin2net <denisdumitriu95@gmail.com>
2025-11-30 17:22:46 +0200

    inceput category

--- app/src/main/java/com/example/sharoma_finder/repository/DashboardRepository.kt
+++ app/src/main/java/com/example/sharoma_finder/repository/DashboardRepository.kt
@@ -3,6 +3,7 @@ package com.example.sharoma_finder.repository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.sharoma_finder.domain.CategoryModel
+import com.example.sharoma_finder.viewModel.BannerModel
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -33,4 +34,28 @@ class DashboardRepository {
         })
         return listData
     }
+
+    fun loadBanner(): LiveData<MutableList<BannerModel>>{
+        val listData=MutableLiveData<MutableList<BannerModel>>()
+        val ref=firebaseDatabase.getReference("Banner")
+        ref.addValueEventListener(object:ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val list= mutableListOf<BannerModel>()
+                for(childSnapshot in snapshot.children){
+                    val item=childSnapshot.getValue(BannerModel::class.java)
+                    item?.let{
+                        list.add(it)
+                    }
+                }
+                listData.value=list
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+        return listData
+    }
+
 }
---
+++ app/src/main/java/com/example/sharoma_finder/viewModel/BannerModel.kt
@@ -0,0 +1,5 @@
+package com.example.sharoma_finder.viewModel
+
+data class BannerModel(
+    val image: String=""
+)
